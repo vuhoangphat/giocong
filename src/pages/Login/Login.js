a8327468e71d0c3643b97696d1f637802c82a129
jarvis-se <74444085+jarvis-se@users.noreply.github.com>
2021-02-22 10:09:25 +0700

    first commit

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './Login.scss';
+import { Input, PageHeader, Form, Button, Checkbox } from 'antd';
+const layout = {
+    labelCol: {
+        span: 8
+    },
+    wrapperCol: {
+        span: 16
+    },
+}
+const tailLayout = {
+    wrapperCol: {
+        offset: 8,
+        span: 16,
+    },
+};
+const Login = () => {
+    return (
+        <div className='Login'>
+            <PageHeader title='Login' />
+            <div className='form'>
+                <Form
+                    {...layout}
+                    name="basic"
+                    initialValues={{
+                        remember: true,
+                    }}
+                >
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your username!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+                    <Form.Item {...tailLayout}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    )
+}
+export default Login;
